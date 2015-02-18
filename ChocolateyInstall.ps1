a86ca6d0cc05bf324d624b5e71dc8081d06e1510
Christopher MANEU <christopher@maneu.net>
2015-02-18 17:07:44 +0100

    Add base files

---
+++ ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+$name   = 'NetMicroframework'
+$url = 'https://netmf.codeplex.com/downloads/get/1423116'
+$url64 = 'https://netmf.codeplex.com/downloads/get/1423116'
+$silent = '/quiet'
+
+Install-ChocolateyPackage $name 'msi' $silent $url $url64
